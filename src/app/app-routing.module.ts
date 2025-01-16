de705a0c25f58dd22b5777b27342dc59876c1730
Matheus <matheusjvs_@hotmail.com>
2025-01-16 15:11:17 -0300

    Substituindo rota padrão e criação da página de listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: ListPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent,
   ],
   imports: [
     BrowserModule,
